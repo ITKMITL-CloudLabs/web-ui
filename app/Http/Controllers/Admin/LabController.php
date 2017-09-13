d8c551522465da9bf107a9d6cdd8a35704402890
Tonsai S <t@iton.in.th>
2017-09-13 20:03:09 +0700

    ITCL-7 เพิ่มหน้าแสดงการทดลอง (admin.lab.show)

--- app/Http/Controllers/Admin/LabController.php
+++ app/Http/Controllers/Admin/LabController.php
@@ -50,9 +50,9 @@ class LabController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Lab $lab)
     {
-        //
+        return view('admin.lab.show', compact('lab'));
     }
 
     /**
